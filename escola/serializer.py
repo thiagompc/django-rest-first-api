c55352d130dfe399443c3b1a847f52f00a26876e
thiagompc <thiago099carvalho@gmail.com>
2021-07-07 01:46:35 -0300

    Create alunos and cursos routers

--- escola/serializer.py
+++ escola/serializer.py
@@ -6,6 +6,7 @@ class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = ['id', 'nome', 'rg', 'cpf', 'data_nascimento']
+        
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
