66011b1a91f10c34c5cd69f3593fb396b52ed034
thiagompc <thiago099carvalho@gmail.com>
2021-07-09 23:25:47 -0300

    Create matricula serializer, viewset and url

--- escola/serializer.py
+++ escola/serializer.py
@@ -1,6 +1,6 @@
 from django.db.models import fields
 from rest_framework import serializers
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +11,8 @@ class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
